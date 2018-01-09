58e1b9bc8ef7a623fe08399451e592957f2dd6f0
Padon Mungsanti <topyttac@outlook.com>
2018-01-09 15:11:54 +0700

    Initial commit

---
+++ imports/ui/LinksListFilters.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Tracker } from 'meteor/tracker';
+import { Session } from 'meteor/session';
+
+export default class LinksListFilters extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showVisible: false
+    };
+  }
+  componentDidMount() {
+    this.tracker = Tracker.autorun(() => {
+      this.setState({
+        showVisible: Session.get('showVisible')
+      });
+    });
+  }
+  componentWillUnmount() {
+    this.tracker.stop();
+  }
+  render() {
+    return (
+      <div>
+        <label className="checkbox">
+          <input className="checkbox__box" type = "checkbox" checked = {!this.state.showVisible} onChange = {(e) => {
+            Session.set('showVisible', !e.target.checked)
+          }}/>
+          show hidden links
+        </label>
+      </div>
+    );
+  }
+}
